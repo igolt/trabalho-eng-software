e912a94eacbff8035dd4e0c7b2c9c9dfe25c6536
Igor Correa Rodrigues <igorcorrearodrigues@gmail.com>
2022-06-29 01:44:38 -0300

    feat: Novo formato para representas zonas em JSON.

--- codigo/src/GameZone.ts
+++ codigo/src/GameZone.ts
@@ -18,7 +18,7 @@ type DoorInfo = {
 type GraphicalMap = Array<number>;
 type CollisionMap = Array<number>;
 
-export interface IZone {
+interface IZoneBase {
   id: string;
 
   carrots: Array<CarrotInfo>;
@@ -27,17 +27,19 @@ export interface IZone {
 
   columns: number;
   rows: number;
+}
 
+export type IZone = IZoneBase & {
   graphicalMap: GraphicalMap;
   collisionMap: CollisionMap;
-}
+};
+
+type ZoneInfo = IZoneBase & {
+  graphicalMap: Array<[number, number, number]>;
+};
 
 const ZONE_PREFIX = "../zones/zone";
 const ZONE_SUFFIX = ".json";
-
-// TODO(igolt): fazer a validação da zona
-const validateZone = (_: any): _ is IZone => true;
-
 const _zoneURL = (id: string) => ZONE_PREFIX + id + ZONE_SUFFIX;
 
 const zones: { readonly [statusCode: string]: string | null } = {
@@ -45,20 +47,61 @@ const zones: { readonly [statusCode: string]: string | null } = {
   "05": _zoneURL("05"),
 };
 
-export const requestZoneFromJSON = async (
-  assetsManager: AssetsManager,
-  zoneId: string
-): Promise<IZone> => {
+const _getZoneUrlById = (zoneId: string) => {
   const zoneUrl = zones[zoneId];
 
   if (zoneUrl == null) {
     throw new Error("requestZoneFromJSON: invalid zone ID");
   }
+  return zoneUrl;
+};
 
-  const zone = await assetsManager.getOrLoadJSON(zoneId.toString(), zoneUrl);
+// TODO(igolt): fazer a validação da zona
+const isZoneInfo = (_: any): _ is ZoneInfo => true;
 
-  if (validateZone(zone)) {
+// TODO(igolt): fazer a validação da zona
+const isZone = (obj: any): obj is IZone => {
+  if (!("graphicalMap" in obj)) {
+    return false;
+  }
+  const graphicalMap = obj["graphicalMap"];
+  const firstElement = graphicalMap[0];
+
+  if (firstElement == null || firstElement == undefined) {
+    return true;
+  }
+  return typeof firstElement == "number";
+};
+
+// TODO(igolt): fazer função de conversão
+const zoneInfoToZone = (zoneInfo: ZoneInfo): IZone => {
+  const newGraphicalMap: number[] = [];
+  const collisionMap: number[] = [];
+
+  zoneInfo.graphicalMap.forEach(value => {
+    newGraphicalMap.push(value[0] * zoneInfo.columns + value[1]);
+    collisionMap.push(value[2]);
+  });
+
+  (zoneInfo as any).graphicalMap = newGraphicalMap;
+  zoneInfo = Object.assign(zoneInfo, { collisionMap });
+
+  return zoneInfo as any as IZone;
+};
+
+export const requestZoneFromJSON = async (
+  assetsManager: AssetsManager,
+  zoneId: string
+): Promise<IZone> => {
+  const zoneUrl = _getZoneUrlById(zoneId);
+  const zone = await assetsManager.getOrLoadJSON(zoneId, zoneUrl);
+
+  if (isZone(zone)) {
+    console.log("loading old zone format");
     return zone;
+  } else if (isZoneInfo(zone)) {
+    console.log("loading new zone format");
+    return zoneInfoToZone(zone);
   }
   throw new Error("requestZoneFromJSON: invalid JSON format for zone.");
 };
