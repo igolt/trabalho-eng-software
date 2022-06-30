e5fe3954f66e2a1a7f130e4dcf8ee9a58065df5b
Igor Correa Rodrigues <igorcorrearodrigues@gmail.com>
2022-06-30 01:07:48 -0300

    feat: Configurando o eslint.

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  ignorePatterns: [".eslintrc.js", "webpack.config.js"],
+  env: {
+    es6: true,
+  },
+  parserOptions: {
+    sourceType: "module",
+  },
+  rules: {
+    "@typescript-eslint/no-empty-function": "warn",
+    "@typescript-eslint/no-unused-vars": "error",
+    camelcase: "error",
+    "no-console": "warn",
+    "no-nested-ternary": "error",
+    "no-redeclare": "error",
+  },
+};
